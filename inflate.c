eb3000f94edba63a61656f99d874c914463f7ad7
Dougall Johnson <dougallj@gmail.com>
2022-08-19 14:46:45 +1000

    Inflate: put "bits" in LSB of "code" struct on little-endian (~3.5% speedup)

--- inflate.c
+++ inflate.c
@@ -365,8 +365,8 @@ void makefixed()
     low = 0;
     for (;;) {
         if ((low % 7) == 0) printf("\n        ");
-        printf("{%u,%u,%d}", (low & 127) == 99 ? 64 : state.lencode[low].op,
-               state.lencode[low].bits, state.lencode[low].val);
+        printf("{%u,%u,%d}", state.lencode[low].bits, state.lencode[low].op,
+               state.lencode[low].val);
         if (++low == size) break;
         putchar(',');
     }
@@ -376,7 +376,7 @@ void makefixed()
     low = 0;
     for (;;) {
         if ((low % 6) == 0) printf("\n        ");
-        printf("{%u,%u,%d}", state.distcode[low].op, state.distcode[low].bits,
+        printf("{%u,%u,%d}", state.distcode[low].bits, state.distcode[low].op,
                state.distcode[low].val);
         if (++low == size) break;
         putchar(',');
